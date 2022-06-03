025612dd11c644f5b098a1e843eeb44302341c6f
sorrowe <s.orrowe@gmail.com>
2022-06-03 20:41:41 +0100

    Allow for custom flag and preserve settings

--- __init__.py
+++ __init__.py
@@ -1,41 +1,82 @@
 
 from tidal import *
-from app import TextApp
-from .drawfuncs import make_pretty, COLOUR_SETS
+from app import TextApp, MenuApp
+from .drawfuncs import make_pretty, COLOUR_SETS, colour_list_to_string, COLOUR_SET_SETTINGS_KEY
 import settings
+from buttons import Buttons
+from textwindow import TextWindow
 
 import vga2_8x8 as default_font
 
 DEFAULT_CUSTOM = COLOUR_SETS[0]
 
-class MakePretty(TextApp):
+DEFAULT_COLOUR_SET = "EMFPride_default_colour_set"
+DEFAULT_JUMP = "EMFPride_default_jump"
+DEFAULT_ANGLE = "EMFPride_default_angle"
+
+class EMFPrideSwirl(MenuApp):
+    TITLE = "EMF PRIDE SWIRL"
 
     def __init__(self):
         super().__init__()
-        self.angle = 50
-        self.jump = 2
-        self.colour_set = 0
+        self.draw_window = None
+        self.setting_window = None
+        choices = (
+            ("DRAW", self.do_draw),
+            ("CUSTOM", self.do_settings),
+            ("HELP", lambda: print("Selected HELP!")),
+        )
 
-    def on_activate(self):
-        super().on_activate() # This will clear the screen by calling TextWindow.redraw()
-        self.do_display()
+        self.window.set_choices(choices)
 
-    def on_start(self):
-        super().on_start()
-        self.buttons.on_press(JOY_CENTRE, self.do_display)
-        self.buttons.on_press(JOY_LEFT, lambda: self.angle_change(-10))
-        self.buttons.on_press(JOY_RIGHT, lambda: self.angle_change(10))
-        self.buttons.on_press(JOY_UP, lambda: self.jump_change(1))
-        self.buttons.on_press(JOY_DOWN, lambda: self.jump_change(-1))
+    def do_draw(self):
+        if not self.draw_window:
+            new_buttons = Buttons()
+            new_buttons.on_press(BUTTON_B, self.pop_window)
+            self.draw_window = ColoursWindow(buttons=new_buttons)
 
-        self.buttons.on_press(BUTTON_A, self.change_colour_set)
-        #self.buttons.on_press(BUTTON_B, self.set_custom)
+        self.push_window(self.draw_window, activate=True)
+        self.window.do_display()
+    
+    def do_settings(self):
+
+        custom_colours = settings.get(COLOUR_SET_SETTINGS_KEY)
+        if not custom_colours:
+            custom_colours = colour_list_to_string(COLOUR_SETS[0][0])
+        
+        new_colours = self.keyboard_prompt("hex list:", custom_colours)
+        if new_colours != custom_colours:
+            settings.set(COLOUR_SET_SETTINGS_KEY, new_colours)
+            settings.save()
+
+
+class ColoursWindow(TextWindow):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.angle = settings.get(DEFAULT_ANGLE, 50)
+        self.jump = settings.get(DEFAULT_JUMP, 2)
+        self.colour_set = settings.get(DEFAULT_COLOUR_SET, 1)
+        if self.buttons:
+            buttons = self.buttons
+            buttons.on_press(JOY_CENTRE, self.do_display)
+            buttons.on_press(JOY_LEFT, lambda: self.angle_change(-10))
+            buttons.on_press(JOY_RIGHT, lambda: self.angle_change(10))
+            buttons.on_press(JOY_UP, lambda: self.jump_change(1))
+            buttons.on_press(JOY_DOWN, lambda: self.jump_change(-1))
+
+            buttons.on_press(BUTTON_A, self.change_colour_set)
+
         
     def angle_change(self, change):
         self.angle = (self.angle + change) % 360
+        settings.set(DEFAULT_ANGLE, self.angle)
+        settings.save()
 
     def jump_change(self, change):
         self.jump = max(1, self.jump + change)
+        settings.set(DEFAULT_JUMP, self.jump)
+        settings.save()
 
     def do_display(self):
         make_pretty(self.angle, self.jump, self.colour_set)
@@ -44,21 +85,11 @@ class MakePretty(TextApp):
         self.colour_set = (self.colour_set + 1) % len(COLOUR_SETS)
         display.fill_rect(0, 230, 135, 10, BLACK)
         display.text(default_font, "{}:{}".format(self.colour_set, self.get_colour_name()), 1, 231, WHITE)
+        settings.set(DEFAULT_COLOUR_SET, self.colour_set)
+        settings.save()
 
     def get_colour_name(self):
         return COLOUR_SETS[self.colour_set][1]
 
-    def set_to_string(self, colour_set):
-        colours = colour_set[0]
-        string_list = ['#%02x%02x%02x' % x for x in colours]
-        return ", ".join(string_list)
 
-    def set_custom(self):
-        current_colours = self.set_to_string(0)
-        new_colours = self.keyboard_prompt("hex, comma seperated:", current_colours, multiline_allowed=True)
-        if new_colours != current_colours:
-            settings.set("custom_flag", new_colours)
-            settings.save()
-
-
-main = MakePretty
+main = EMFPrideSwirl
--- drawfuncs.py
+++ drawfuncs.py
@@ -1,12 +1,27 @@
 
 from tidal import *
 from math import radians, floor
+import settings
+import vga2_8x8 as default_font
 
 MAX_HEIGHT = 240
 MAX_WIDTH = 135
 
+COLOUR_SET_SETTINGS_KEY = "EMFPride_custom_pride_flag"
+
 
 COLOUR_SETS = (
+    (
+        (
+            (255, 0, 24),
+            (255, 165, 44),
+            (255, 255, 65),
+            (0, 128, 24),
+            (0, 0, 249),
+            (134, 0, 125),
+        ),
+        "Custom"
+    ),
     (
         (
             (255, 0, 24),
@@ -83,8 +98,17 @@ COLOUR_SETS = (
 def get_angle(i, steps, sweep):
     return radians((i/steps) * sweep);
 
-def get_colour(i, set):
-    colour_list = COLOUR_SETS[set][0]
+def get_colour_list(i):
+    colour_list = COLOUR_SETS[i][0]
+    if i == 0:
+        custom_colours = settings.get(COLOUR_SET_SETTINGS_KEY)
+        if custom_colours:
+            new_list = string_to_colour_list(custom_colours)
+            if len(new_list) > 0:
+                colour_list = new_list
+    return colour_list
+
+def get_colour(i, colour_list):
     return color565(*colour_list[i % len(colour_list)])
 
 def draw_square(w_off, h_off, colour, rotation=0):
@@ -99,16 +123,41 @@ def draw_square(w_off, h_off, colour, rotation=0):
     display.fill_polygon(poly, 0, 0, colour, rotation, floor(MAX_WIDTH/2), floor(MAX_HEIGHT/2))
 
 
-def make_pretty(angle_sweep = 50, jump = 2, set = 0):
+def make_pretty(angle_sweep = 50, jump = 2, colour_set = 0):
     
     steps = floor(MAX_WIDTH/(2*jump))
     h_step = (MAX_HEIGHT/2)/steps
     
-    display.fill(get_colour(0, set))
+    colour_list = get_colour_list(colour_set)
+    display.fill(get_colour(0, colour_list))
     for i in range(0,steps):
         angle = get_angle(i, steps, angle_sweep)
-        #x_offset = i*jump
-        #y_offset = round(i*h_step)
-        #display.draw_rect(x_offset, y_offset, MAX_WIDTH-2*x_offset, MAX_HEIGHT - 2*y_offset, get_colour(i))
-        draw_square(i*jump, round(i*h_step), get_colour(i, set), angle)
+        draw_square(i*jump, round(i*h_step), get_colour(i, colour_list), angle)
 
+
+def colour_list_to_string(colour_list):
+    string_list = ['#%02x%02x%02x' % x for x in colour_list]
+    return ", ".join(string_list)
+
+def string_to_colour_list(colour_string):
+    colour_list = []
+    hex_list = colour_string.split(",")
+    for hex_string in hex_list:
+        rgb = read_hex_string(hex_string)
+        if rgb:
+            colour_list.append(rgb)
+    
+    return colour_list
+
+def read_hex_string(hex_str):
+    hex_str = hex_str.strip()
+    if len(hex_str) == 7 and hex_str[0] == "#":
+        hex_str = hex_str[1:]
+    if len(hex_str) == 6:
+        try:
+            rgb = (int(hex_str[0:2], 16), int(hex_str[2:4], 16), int(hex_str[4:6],16))
+        except:
+            return None
+        
+        return rgb
+    return None
