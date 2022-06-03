baf8a3de5d91361dd9fa71ede0d56789f95f86cd
sorrowe <s.orrowe@gmail.com>
2022-06-03 16:42:29 +0100

    updated to add various pride swirls

--- __init__.py
+++ __init__.py
@@ -1,25 +1,35 @@
-from tidal import *
-from app import App
-from .drawfuncs import make_pretty
 
-class MakePretty(App):
+from tidal import *
+from app import TextApp
+from .drawfuncs import make_pretty, COLOUR_SETS
+import settings
+
+import vga2_8x8 as default_font
+
+DEFAULT_CUSTOM = COLOUR_SETS[0]
+
+class MakePretty(TextApp):
 
     def __init__(self):
         super().__init__()
         self.angle = 50
         self.jump = 2
+        self.colour_set = 0
 
     def on_activate(self):
         super().on_activate() # This will clear the screen by calling TextWindow.redraw()
         self.do_display()
 
+    def on_start(self):
+        super().on_start()
         self.buttons.on_press(JOY_CENTRE, self.do_display)
         self.buttons.on_press(JOY_LEFT, lambda: self.angle_change(-10))
         self.buttons.on_press(JOY_RIGHT, lambda: self.angle_change(10))
         self.buttons.on_press(JOY_UP, lambda: self.jump_change(1))
         self.buttons.on_press(JOY_DOWN, lambda: self.jump_change(-1))
 
-        #self.buttons.on_press(BUTTON_A, self.change_colour_set)
+        self.buttons.on_press(BUTTON_A, self.change_colour_set)
+        #self.buttons.on_press(BUTTON_B, self.set_custom)
         
     def angle_change(self, change):
         self.angle = (self.angle + change) % 360
@@ -28,10 +38,27 @@ class MakePretty(App):
         self.jump = max(1, self.jump + change)
 
     def do_display(self):
-        make_pretty(self.angle, self.jump)
+        make_pretty(self.angle, self.jump, self.colour_set)
 
-    #def change_colour_set(self):
+    def change_colour_set(self):
+        self.colour_set = (self.colour_set + 1) % len(COLOUR_SETS)
+        display.fill_rect(0, 230, 135, 10, BLACK)
+        display.text(default_font, "{}:{}".format(self.colour_set, self.get_colour_name()), 1, 231, WHITE)
 
+    def get_colour_name(self):
+        return COLOUR_SETS[self.colour_set][1]
+
+    def set_to_string(self, colour_set):
+        colours = colour_set[0]
+        string_list = ['#%02x%02x%02x' % x for x in colours]
+        return ", ".join(string_list)
+
+    def set_custom(self):
+        current_colours = self.set_to_string(0)
+        new_colours = self.keyboard_prompt("hex, comma seperated:", current_colours, multiline_allowed=True)
+        if new_colours != current_colours:
+            settings.set("custom_flag", new_colours)
+            settings.save()
 
 
 main = MakePretty
--- drawfuncs.py
+++ drawfuncs.py
@@ -1,17 +1,91 @@
+
 from tidal import *
 from math import radians, floor
 
 MAX_HEIGHT = 240
 MAX_WIDTH = 135
 
-COLOUR_LIST = [RED, BRAND_ORANGE, BRAND_YELLOW, GREEN, BLUE, MAGENTA, ADDITIONAL_PURPLE]
-COLOUR_LIST_2 = [RED, BLUE]
+
+COLOUR_SETS = (
+    (
+        (
+            (255, 0, 24),
+            (255, 165, 44),
+            (255, 255, 65),
+            (0, 128, 24),
+            (0, 0, 249),
+            (134, 0, 125),
+        ),
+        "Classic"
+    ),
+    (
+        (
+            (255, 0, 24),
+            (255, 165, 44),
+            (255, 255, 65),
+            (0, 128, 24),
+            (0, 0, 249),
+            (134, 0, 125),
+            (255, 255, 255),
+            (245, 169, 184),
+            (91, 206, 250),
+            (97, 54, 2),
+            (0, 0, 0),
+        ),
+        "Progress"
+    ),
+    (
+        (
+            (91, 206, 250),
+            (245, 169, 184),
+            (255, 255, 255),
+            (245, 169, 184),
+            (91, 206, 250),
+        ),
+        "Trans"
+    ),
+    (
+        (
+            (255, 244, 48),
+            (255, 255, 255),
+            (156, 89, 209),
+            (0, 0, 0),
+        ),
+        "NB"
+    ),
+    (
+        (
+            (255, 27, 141),
+            (255, 218, 0),
+            (27, 179, 255),
+        ),
+        "Pan"
+    ),
+    (
+        (
+            (208, 0, 112),
+            (140, 71, 153),
+            (0, 50, 160),
+        ),
+        "Bi"
+    ),
+    (
+        (
+            (0, 0, 0),
+            (164, 164, 164),
+            (255, 255, 255),
+            (129, 0, 129),
+        ),
+        "Asexual"
+    ),
+)
 
 def get_angle(i, steps, sweep):
     return radians((i/steps) * sweep);
 
-def get_colour(i):
-    return COLOUR_LIST[i % len(COLOUR_LIST)]
+def get_colour(i, set):
+    colour_list = COLOUR_SETS[set][0]
+    return color565(*colour_list[i % len(colour_list)])
 
 def draw_square(w_off, h_off, colour, rotation=0):
     poly = [
@@ -25,16 +99,16 @@ def draw_square(w_off, h_off, colour, rotation=0):
     display.fill_polygon(poly, 0, 0, colour, rotation, floor(MAX_WIDTH/2), floor(MAX_HEIGHT/2))
 
 
-def make_pretty(angle_sweep = 50, jump = 2):
+def make_pretty(angle_sweep = 50, jump = 2, set = 0):
     
     steps = floor(MAX_WIDTH/(2*jump))
     h_step = (MAX_HEIGHT/2)/steps
     
-    display.fill(CYAN)
+    display.fill(get_colour(0, set))
     for i in range(0,steps):
         angle = get_angle(i, steps, angle_sweep)
         #x_offset = i*jump
         #y_offset = round(i*h_step)
         #display.draw_rect(x_offset, y_offset, MAX_WIDTH-2*x_offset, MAX_HEIGHT - 2*y_offset, get_colour(i))
-        draw_square(i*jump, round(i*h_step), get_colour(i), angle)
+        draw_square(i*jump, round(i*h_step), get_colour(i, set), angle)
 
